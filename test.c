2c386e649998dbf5c9e95b8eeaffb5fd2130c96d
Dale Smith <dtsmith@mindspring.com>
2018-07-09 17:45:28 +0000

    memory leak

--- test.c
+++ test.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#define ARR_SIZE 10
+
 struct test_s {
     char *first;
     char *second;
@@ -9,17 +11,18 @@ struct test_s {
 };
 
 int main(int argc, char *argv[]) {
-    char* string = (char *)malloc(5 * sizeof(char));
-    fprintf(stderr, "String is '%s'\n", string);
-//    struct test_s ta[10];
-//    char *msg = (char *)malloc(10001*sizeof(char));
-//    for (size_t i = 0; i < 10; i++) {
-//	ta[i].first = (char *)"A";
-	//char message[1000];
-//	sprintf(msg, (char *)"A big long test string for i = %lu\n", i);
-//	ta[i].second = msg;
-//	ta[i].type = 1;
-//    }
+    struct test_s my_test[ARR_SIZE];
+    char *msg = (char *)malloc(10001*sizeof(char));
+    my_test[0].first = (char *)"A String";
+    my_test[0].second = (char *)"Second string";
+    my_test[0].type = 0;
+    for (size_t i = 1; i < ARR_SIZE; i++) {
+	my_test[i].first = (char *)"A";
+	sprintf(msg, (char *)"A big long test string for i = %lu\n", i);
+	my_test[i].second = msg;
+	my_test[i].type = 1;
+	fprintf(stderr, "%s\n", my_test[i].first);
+    }
     return 0;
 }
 
